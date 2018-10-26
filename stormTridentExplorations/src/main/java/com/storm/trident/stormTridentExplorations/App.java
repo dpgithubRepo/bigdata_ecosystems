d1847d70ff7b6b600bed977dce7984affe91bedd
Durga Prasad <durga.pras10@gmail,com>
2018-10-26 09:22:04 +0530

    Storm Trident Introduction doc added

---
+++ stormTridentExplorations/src/main/java/com/storm/trident/stormTridentExplorations/App.java
@@ -0,0 +1,13 @@
+package com.storm.trident.stormTridentExplorations;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
