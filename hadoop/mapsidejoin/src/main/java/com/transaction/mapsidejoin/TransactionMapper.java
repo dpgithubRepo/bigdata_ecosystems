c390e7bad958fa3005ccf6484557bf9610d89eb2
Durga Prasad <dpgithubRepo@users.noreply.github.com>
2018-11-03 10:49:49 +0530

    Update TransactionMapper.java

--- hadoop/mapsidejoin/src/main/java/com/transaction/mapsidejoin/TransactionMapper.java
+++ hadoop/mapsidejoin/src/main/java/com/transaction/mapsidejoin/TransactionMapper.java
@@ -1,8 +1,6 @@
 package com.transaction.mapsidejoin;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
@@ -14,47 +12,59 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+/**
+ * The Class TransactionMapper.
+ *
+ * @author Durga Prasad
+ */
 public class TransactionMapper extends Mapper<LongWritable, Text, Text, Text>{
 
-	Map<String, String> userInfo = new HashMap<String, String>();
-	
-	
-	//load the users file from distributed cache to memory
-	@Override
-	protected void setup(Mapper<LongWritable, Text, Text, Text>.Context context)
-			throws IOException, InterruptedException {
-		
-		
-		Path[] files = DistributedCache.getLocalCacheFiles(context.getConfiguration());
-		for(Path file : files){
-			if(file.getName().equals("users.txt")){
-				BufferedReader reader = new BufferedReader(new FileReader(file.toString()));
-				String line = null;
-				while((line= reader.readLine())!=null){
-					String[] cols = line.split("\\s+");
-					String usrId = cols[0];
-					String emailId = cols[2];
-					userInfo.put(usrId, emailId);
-				}
-				
-			}
-		}
-	}
-	
-	
-	//001 Robin robin_stuart1@gmail.com   -- user file
-	//t002 001 Food debit NYC -- transaction file
-	// let us build out put like transactionid usermailid
-	// t001 stuart1@gmail.com 
-		
-	@Override
-	protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, Text>.Context context)
-			throws IOException, InterruptedException {
-		
-		String[] transactionCols = value.toString().split("\\s+");
-		if(transactionCols!=null && transactionCols.length >2)
-			context.write(new Text(transactionCols[0]), new Text(userInfo.get(transactionCols[1])));
-	}
-	
-	
+        /** The user info. */
+        Map<String, String> userInfo = new HashMap<String, String>();
+
+
+        /* (non-Javadoc)
+         * @see org.apache.hadoop.mapreduce.Mapper#setup(org.apache.hadoop.mapreduce.Mapper.Context)
+         */
+        //load the users file from distributed cache to memory
+        @Override
+        protected void setup(Mapper<LongWritable, Text, Text, Text>.Context context)
+                        throws IOException, InterruptedException {
+
+
+                Path[] files = DistributedCache.getLocalCacheFiles(context.getConfiguration());
+                for(Path file : files){
+                        if(file.getName().equals("users.txt")){
+                                BufferedReader reader = new BufferedReader(new FileReader(file.toString()));
+                                String line = null;
+                                while((line= reader.readLine())!=null){
+                                        String[] cols = line.split("\\s+");
+                                        String usrId = cols[0];
+                                        String emailId = cols[2];
+                                        userInfo.put(usrId, emailId);
+                                }
+
+                        }
+                }
+        }
+
+
+        //001 Robin robin_stuart1@gmail.com   -- user file
+        //t002 001 Food debit NYC -- transaction file
+        // let us build out put like transactionid usermailid
+        // t001 stuart1@gmail.com
+
+        /* (non-Javadoc)
+         * @see org.apache.hadoop.mapreduce.Mapper#map(KEYIN, VALUEIN, org.apache.hadoop.mapreduce.Mapper.Context)
+         */
+        @Override
+        protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, Text>.Context context)
+                        throws IOException, InterruptedException {
+
+                String[] transactionCols = value.toString().split("\\s+");
+                if(transactionCols!=null && transactionCols.length >2)
+                        context.write(new Text(transactionCols[0]), new Text(userInfo.get(transactionCols[1])));
+        }
+
+
 }
